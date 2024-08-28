24506f01c135ea439386faf6f4ca9b17e7831ce2
Juan Carlos Cajas <158212612+JuanCarlosCajas@users.noreply.github.com>
2024-08-27 21:18:52 -0500

    proyect decidijc

--- connection/conectionClass.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Conection extends mysqli{
-  function __construct(){
-    parent::__construct('localhost', 'root', '', 'api');
-    $this->set_charset('utf8');
-    $this->connect_error == null ? 'Conexion exitosa' : die('Error al conectar');
-  }
-}
--- index.php
+++ index.php
@@ -1,25 +1,17 @@
 <?php
-$request_URL = $_SERVER['REQUEST_URI'];
-$folderPath = dirname($_SERVER['SCRIPT_NAME']);
-
-if(isset($_SERVER['QUERY_STRING'])){
-  $directorio = $_SERVER['QUERY_STRING'];
-  $request_URL = str_replace("?".$directorio,"",$request_URL);
-}
-
-$url = substr($request_URL, strlen($folderPath));
+require "config.php";
 
 /*echo "<p>$url</p>";*/
 
 switch($url){
   case '/':
-    require 'routes/main.php';
+    require 'controller/main.php';
     break;
   case '/productos':
-    require 'routes/producto.php';
+    require 'controller/producto.php';
     break;
   case '/adminProducto':
-    require 'routes/adminProducto.php';
+    require 'controller/adminProducto.php';
     break;
   case '/prueba':
     echo "https://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
